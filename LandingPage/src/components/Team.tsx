8126ab0976b16e3f16b1a447884c30a317dfb372
PPT1001 <pragash1001@gmail.com>
2024-03-27 22:56:06 +0530

    Updated content

--- LandingPage/src/components/Team.tsx
+++ LandingPage/src/components/Team.tsx
@@ -1,7 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -9,12 +8,11 @@ import {
 } from "@/components/ui/card";
 import { Instagram, Linkedin, Github } from "lucide-react";
 
-import Pragash from '../assets/Pragesh.jpg';
-import Aashif from '../assets/Aashif.jpg';
-import Ishma from '../assets/Ishma.jpg';
-import Santosh from '../assets/Santosh.jpeg';
-import Tharoosh from '../assets/Tharoosh.jpg';
-
+import Pragash from "../assets/Pragesh.jpg";
+import Aashif from "../assets/Aashif.jpg";
+import Ishma from "../assets/Ishma.jpg";
+import Santosh from "../assets/Santosh.jpeg";
+import Tharoosh from "../assets/Tharoosh.jpg";
 
 interface TeamProps {
   imageUrl: string;
@@ -33,10 +31,13 @@ const teamList: TeamProps[] = [
   {
     imageUrl: Pragash,
     name: "Pragash Sasitharan",
-    position: "Team Leader",
+    position: "DevOps Developer",
     description: "You can't see me",
     socialNetworks: [
-      { name: "Linkedin", url: "https://www.linkedin.com/in/pragash-sasitharan/" },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/pragash-sasitharan/",
+      },
       {
         name: "Instagram",
         url: "https://www.instagram.com/pragash_.s/",
@@ -51,7 +52,7 @@ const teamList: TeamProps[] = [
   {
     imageUrl: Aashif,
     name: "Mohamed Aashif",
-    position: "Machine Learning Developer",
+    position: "ML Developer",
     description: "You can't see me",
     socialNetworks: [
       { name: "Linkedin", url: "https://www.linkedin.com/in/mohamed-aashif/" },
@@ -68,7 +69,7 @@ const teamList: TeamProps[] = [
   {
     imageUrl: Tharoosh,
     name: "Sudesh Tharoosh",
-    position: "Web Developer",
+    position: "Frontend Developer",
     description: "You can't see me",
     socialNetworks: [
       { name: "Linkedin", url: "https://www.linkedin.com/in/sudeshtharoosh/" },
@@ -85,7 +86,7 @@ const teamList: TeamProps[] = [
   {
     imageUrl: Ishma,
     name: "Mohamed Ishma",
-    position: "Machince Leaning Developer",
+    position: "Full Stack Developer",
     description: "You can't see me",
     socialNetworks: [
       { name: "Linkedin", url: "www.linkedin.com/in/ishma-ifan-179428228" },
@@ -102,10 +103,13 @@ const teamList: TeamProps[] = [
   {
     imageUrl: Santosh,
     name: "Santosh Manoharadas",
-    position: "Web Developer",
+    position: "Frontend Developer",
     description: "You can't see me",
     socialNetworks: [
-      { name: "Linkedin", url: "https://www.linkedin.com/in/santosh-manoharadas/" },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/santosh-manoharadas/",
+      },
       {
         name: "Instagram",
         url: "https://www.instagram.com/santosh.manoharadas/",
@@ -133,12 +137,13 @@ export const Team = () => {
   };
 
   return (
-    <section id="team" className="container py-24 sm:py-20 flex flex-col items-center justify-center">
+    <section
+      id="team"
+      className="container py-24 sm:py-20 flex flex-col items-center justify-center"
+    >
       <h2 className="text-3xl md:text-4xl font-bold mb-10">
         {/* <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text"> */}
-        <span className="text-[#1D7D81]">
-          Our Dedicated{" "}
-        </span>
+        <span className="text-[#1D7D81]">Our Dedicated </span>
         Crew
       </h2>
 
@@ -149,7 +154,7 @@ export const Team = () => {
 
       <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-8 gap-y-10">
         {teamList.map(
-          ({ imageUrl, name, position, description, socialNetworks }: TeamProps) => (
+          ({ imageUrl, name, position, socialNetworks }: TeamProps) => (
             <Card
               key={name}
               className="bg-muted/50 relative mt-16 flex flex-col justify-center items-center"
@@ -160,16 +165,11 @@ export const Team = () => {
                   alt={`${name} ${position}`}
                   className="absolute -top-12 rounded-full w-24 h-24 aspect-square object-cover"
                 />
-                <CardTitle className="text-center">{name}</CardTitle>
-                <CardDescription className="text-primary font-semibold">
+                <CardTitle className="text-center leading-7">{name}</CardTitle>
+                <CardDescription className="text-primary text-center font-semibold">
                   {position}
                 </CardDescription>
               </CardHeader>
-
-              <CardContent className="text-center pb-2">
-                <p>{description}</p>
-              </CardContent>
-
               <CardFooter>
                 {socialNetworks.map(({ name, url }: SociaNetworkslProps) => (
                   <div key={name}>
